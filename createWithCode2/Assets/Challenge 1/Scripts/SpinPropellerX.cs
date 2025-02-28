14db30c5ac09bf7ca2cebbaf6409547c2c776cf0
gagegerding <gagegerding@gmail.com>
2025-02-28 13:05:17 -0600

    large files added

---
+++ createWithCode2/Assets/Challenge 1/Scripts/SpinPropellerX.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpinPropellerX : MonoBehaviour
+{
+    public float propellerRotationSpeed; 
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Rotate(Vector3.forward *  propellerRotationSpeed * Time.deltaTime);
+    }
+}
